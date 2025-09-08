edbac26e46fc0389d13f0e178e09078a759f8030
Pedro Henrique <pedrohenriquedasilvapereira12@gmail.com>
2025-09-07 23:38:01 -0300

    First commit

---
+++ customersmicroservice/src/main/java/io/github/microservices/customersmicroservice/CustomersmicroserviceApplication.java
@@ -0,0 +1,13 @@
+package io.github.microservices.customersmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomersmicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomersmicroserviceApplication.class, args);
+	}
+
+}
---
+++ customersmicroservice/src/test/java/io/github/microservices/customersmicroservice/CustomersmicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.microservices.customersmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomersmicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
